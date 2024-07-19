e245f4e5abf7051fc034c04c9054e4f678ae54b9
Ylleza Merovci <ym51807@ubt-uni.net>
2024-07-19 05:44:20 -1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
+
+const App = () => {
+  return (
+    <>
+     <Navbar />
+     <Hero title='Test Title' subtitle='This is the subtitle' />
+     <HomeCards />
+     <JobListings />
+     <ViewAllJobs />
+
+    
+    </>
+  )
+}
+
+export default App
